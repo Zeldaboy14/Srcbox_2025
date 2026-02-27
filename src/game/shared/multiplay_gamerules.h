21a11fbf3cbea6101fbc91da347b1569661fe650
Zeldaboy14 <zeldaboy1999@gmail.com>
2026-02-27 17:58:07 -0600

    [bugfix] multiplaygamerules think
    wasn't commented out if LUA_SDK isn't defined. client now builds perfectly

--- src/game/shared/multiplay_gamerules.h
+++ src/game/shared/multiplay_gamerules.h
@@ -109,7 +109,9 @@ public:
 		return BaseClass::ShouldDrawHeadLabels();
 	}
 #ifdef CLIENT_DLL
+#ifdef LUA_SDK
 	virtual void Think(void);
+#endif
 #endif
 #ifndef CLIENT_DLL
 	virtual void FrameUpdatePostEntityThink();
